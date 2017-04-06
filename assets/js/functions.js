5495a9bf9c14efb174c40aae181c7af148cfdf9c
Piotr Lasota <lasota.piotr1@gmail.com>
2017-04-06 13:52:21 +0200

    added feature: background changing when window is scrolled

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,12 +4,23 @@
   /* Primary-nav menu
     ========================================================================= */
 
+  const pageHead = document.querySelector('.js-page-head');
+
   const menuBtn = document.querySelector('.js-nav-primary__menu-btn-wrapper');
   const navPrimary = document.querySelector('.js-nav-primary');
 
   function menuBtnHandler(e) {
     navPrimary.classList.toggle('is-open');
     e.stopPropagation();
+  }
+
+  function scrollHandler() {
+    if(window.scrollY >= 50) {
+      console.log(pageHead.classList.add('is-scrolled'));
+    }
+    if(window.scrollY < 50) {
+      console.log(pageHead.classList.remove('is-scrolled'));
+    }
   }
 
   /**
@@ -25,4 +36,6 @@
   });
   document.addEventListener('click', documentClickHandler);
 
+
+  document.addEventListener('scroll', scrollHandler);
 }());
