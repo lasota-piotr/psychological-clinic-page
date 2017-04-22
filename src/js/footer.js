f33f47196e964c8559e1c210665772a9f10b80ec
Piotr Lasota <lasota.piotr1@gmail.com>
2017-04-22 21:28:51 +0200

    footer now gets information from contact.json

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,7 @@
 const footer = require('../_includes/footer.hbs');
+import jsonContact from '../contact.json';
 
 export const renderFooter = function() {
   const footerContainer = document.querySelector('#footer');
-  footerContainer.innerHTML = footer();
+  footerContainer.innerHTML = footer(jsonContact);
 }
