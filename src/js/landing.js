4e40e7d121ef2c21fb3bc8abe9ee835533721424
Piotr Lasota <lasota.piotr1@gmail.com>
2017-04-27 19:13:52 +0200

    added links from json, added clinics info, updated button in hero for scrolling to clinics section

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,8 +1,39 @@
+require('smoothscroll-polyfill').polyfill();
 const landing = require('../_includes/landing.hbs');
+import linksJson from '../links.json';
+import { jsonClinicsWithLinks } from './helpers/jsonClinicsWithLinks';
 
-function renderLanding() {
+
+function renderLanding(jsonData) {
   const landingContainer = document.querySelector('#landing');
-  landingContainer.innerHTML = landing();
+  landingContainer.innerHTML = landing(jsonData);
 }
 
-renderLanding();
+renderLanding(Object.assign(linksJson, jsonClinicsWithLinks));
+
+const clinicsBtnInit = () => {
+  function clinicsBtnHandler(e) {
+    e.preventDefault();
+    
+    const clinicsBtnIdArr = clinicsBtn.href
+                            .split('/')
+                            .find((el) => el.includes('#'))
+                            .split('#');
+    const clinicsBtnId = clinicsBtnIdArr[clinicsBtnIdArr.length - 1];
+    goToId(clinicsBtnId);
+  }
+
+  const clinicsBtn = document.querySelector('.js-clinics-btn');
+  clinicsBtn.addEventListener('click', clinicsBtnHandler);
+
+
+  function goToId(idEl) {
+    // Go to ID
+    document.getElementById(idEl).scrollIntoView({behavior: 'smooth'});
+
+    // Change URL
+    window.history.pushState(null, idEl, '#' + idEl);
+  }
+};
+
+clinicsBtnInit();
