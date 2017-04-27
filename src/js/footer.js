69377e423d33dcc08b4dec5314a47a1fbbffdcee
Piotr Lasota <lasota.piotr1@gmail.com>
2017-04-27 16:51:44 +0200

    anchors now get href from links json

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,14 @@
 const footer = require('../_includes/footer.hbs');
 import jsonContact from '../contact.json';
+import linksJson from '../links.json';
+
+const jsonData = {
+  [Object.keys(jsonContact)[0]] : jsonContact[Object.keys(jsonContact)[0]],
+  [Object.keys(linksJson)[0]] : linksJson[Object.keys(linksJson)[0]]
+}
+
 
 export const renderFooter = function() {
   const footerContainer = document.querySelector('#footer');
-  footerContainer.innerHTML = footer(jsonContact);
+  footerContainer.innerHTML = footer(jsonData);
 }
