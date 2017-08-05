eb08f9be17e4890d374cff5578567b6329c1f96a
Piotr Lasota <lasota.piotr1@gmail.com>
2017-08-05 21:43:19 +0200

    added modal

--- src/js/specialists.js
+++ src/js/specialists.js
@@ -1,3 +1,4 @@
+import VanillaModal from 'vanilla-modal';
 const specialists = require('../_includes/specialists.hbs');
 import jsonSpecialists from '../specialists.json';
 
@@ -7,3 +8,11 @@ function renderspecialists(jsonData) {
 }
 
 renderspecialists(jsonSpecialists);
+
+const modal = new VanillaModal({
+  modal: '.js-modal',
+  modalInner: '.js-modal__inner',
+  modalContent: '.js-modal__content',
+  loadClass: 'has-loaded-modal',
+  class: 'has-visible-modal'
+});
